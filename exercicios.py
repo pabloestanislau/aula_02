76ba5d1aec7c077325ae4fbe2892d799901a6635
pabloestanislau <pablodata25@gmail.com>
2025-07-04 20:47:22 -0300

    adiconando exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -72,6 +72,9 @@
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 
+#nome_completo = input("Digite o seu nome completo: ")
+#nome_minuscula = nome_completo.lower() 
+#print("Nome em minúsculas:", nome_minuscula)
 
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
