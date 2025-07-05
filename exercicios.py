681095fcc6b61faa4c84b8607499a1f33122780a
pabloestanislau <pablodata25@gmail.com>
2025-07-05 12:00:24 -0300

    Adicioanndo exercicios

--- exercicios.py
+++ exercicios.py
@@ -77,11 +77,18 @@
 #print("Nome em minúsculas:", nome_minuscula)
 
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no fina
-frase = input("Digite uma frase: ")
-frase_sem_espacos = frase.strip()
-print("Frase sem espaços no inicio e no final:", frase_sem_espacos)
+#frase = input("Digite uma frase: ")
+#frase_sem_espacos = frase.strip()
+#print("Frase sem espaços no inicio e no final:", frase_sem_espacos)
 
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+data = input("Digite uma data no formato dd/mm/aaaa: ")
+dia, mes, ano = data.split("/")
+print("Dia:", dia)
+print("Mês:", mes)
+print("Ano:", ano)
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
