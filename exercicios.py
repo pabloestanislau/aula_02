4ee6f7349dca6ad765efb6aa058dcc7fec2e2498
pabloestanislau <pablodata25@gmail.com>
2025-07-05 11:38:43 -0300

    Adicinando exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -66,9 +66,9 @@
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 
-#texto = input("Digite um texto: ")
-#texto_maiusculas = texto.upper()
-#print("Texto em maiúsculas:", texto_maiusculas)
+texto = input("Digite um texto: ")
+texto_maiusculas = texto.upper()
+print("Texto em maiúsculas:", texto_maiusculas)
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 
