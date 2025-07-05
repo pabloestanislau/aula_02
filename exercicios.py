c85b357b5980aeebcc5dad9e644d00f04df90e8c
pabloestanislau <pablodata25@gmail.com>
2025-07-05 11:40:19 -0300

    Adicionando exercicios

--- exercicios.py
+++ exercicios.py
@@ -66,15 +66,15 @@
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 
-texto = input("Digite um texto: ")
-texto_maiusculas = texto.upper()
-print("Texto em maiúsculas:", texto_maiusculas)
+#texto = input("Digite um texto: ")
+#texto_maiusculas = texto.upper()
+#print("Texto em maiúsculas:", texto_maiusculas)
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 
-#nome_completo = input("Digite o seu nome completo: ")
-#nome_minuscula = nome_completo.lower() 
-#print("Nome em minúsculas:", nome_minuscula)
+nome_completo = input("Digite o seu nome completo: ")
+nome_minuscula = nome_completo.lower() 
+print("Nome em minúsculas:", nome_minuscula)
 
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
